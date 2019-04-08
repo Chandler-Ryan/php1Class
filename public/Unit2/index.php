b9f8356af72b2851e7160b1085b9a4ed29365cb8
Chandler <chandler.ryan@live.com>
2019-04-08 00:38:05 -0500

    The state i am submitting project

--- public/Unit2/index.php
+++ public/Unit2/index.php
@@ -4,11 +4,63 @@ require_once('includes/header.php');
 
 ?>
 
-<main class="container">
+<div style="margin-top:56px">
+    <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+        <ol class="carousel-indicators">
+            <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+            <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+            <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+            <li data-target="#carouselExampleIndicators" data-slide-to="3"></li>
+        </ol>
+        <div class="carousel-inner">
+            <div class="carousel-item active">
+            <img src="img/Sagan1.jpg" class="d-block w-100 rounded" alt="...">
+                <div class="carousel-caption d-none d-md-block">
+                    <h5><span style="background:black;opacity:0.65;padding:0 10px">Sagan!</span></h5>
+                    <p><span style="background:black;opacity:0.65;padding:0 10px">Photo from roadcycling.co.nz</span></p>
+                </div>
+            </div>
+            <div class="carousel-item">
+            <img src="img/orangeMTB.jpg" class="d-block w-100 rounded" alt="...">
+            </div>
+            <div class="carousel-item">
+            <img src="img/Sagan2.jpg" class="d-block w-100 rounded" alt="...">
+                <div class="carousel-caption d-none d-md-block">
+                    <h5><span style="background:black;opacity:0.65;padding:0 10px">Sagan!</span></h5>
+                    <p><span style="background:black;opacity:0.65;padding:0 10px">Photo from www.trifind.com</span></p>
+                </div>
+            </div>
+            <div class="carousel-item">
+            <img src="img/orangeWheels.jpg" class="d-block w-100 rounded" alt="...">
+            </div>
+        </div>
+        <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="sr-only">Previous</span>
+        </a>
+        <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="sr-only">Next</span>
+        </a>
+    </div>
+</div>
 
+<div class="container">
 
+    <h1 class="text-center p-3">Sunday, April 14th, "Hell of the North"</h1>
+    <p class="text-justifed">
+        Life doesn't get any better then this... On the 14<sup>th</sup> TV doesn't get any better then this.
+        In the morning US time, watch the best cyclists try to conquer the "Hell of the North" and then later
+        watch the king of the north burn some zombies!
+    </p>
+    <p>
+        To gear up for Paris-Roubaix check out our products at the link below:
+    </p>
+    <p>
+        <a href="products.php" class="btn btn-danger" style="margin:auto; display:block; width: 45%;">Products</a>
+    </p>
 
-</main>
+</div>
 
 <?php
 require_once('includes/footer.php');
--- public/Unit2/orders.php
+++ public/Unit2/orders.php
@@ -1,13 +1,81 @@
 <?php 
 $title = "Orders";
 require_once('includes/header.php');
-
+$cList = false;
+if(empty($_GET['id']) || !is_numeric($_GET['id']) || 
+(is_int($_GET['id']) && $_GET['id'] < 1 )){
+    // get all Customers
+    $query = "SELECT FirstName, LastName, CustomerID, City, `State`
+                FROM Customer
+                ORDER BY LastName";
+    // execute the query
+    $result = mysqli_query($db, $query) or die('Error in query: ' . mysqli_error($db));
+    echo 'first';
+    $cList = true;
+}else if(!empty($_GET['id']) && is_numeric($_GET['id']) && $_GET['id'] > 0 ){
+    $query = "SELECT Concat(FirstName, ' ', LastName) as `name`
+    FROM Customer WHERE CustomerID = {$_GET['id']}";
+    $name = mysqli_fetch_array(mysqli_query($db, $query))['name'];
+    // get all orders for this customer
+    $query = "SELECT *
+                FROM OrderHeader oh
+                JOIN OrderDetail od ON oh.OrderID = od.OrderID
+                WHERE oh.CustomerID = {$_GET['id']}";
+    // execute the query
+    $result = mysqli_query($db, $query) or die('Error in query: ' . mysqli_error($db));
+    $orders = mysqli_fetch_array($result);
+    $cList = false;
+}
+// close database connection
+mysqli_close($db);
 ?>
 
 <main class="container">
-
-
-
+    <h1 class="text-center"><?=$cList ? "Customers" : "Orders for $name"?></h1>
+    <table class="table table-striped table-bordered table-hover">
+    <?php if($cList):?>
+        <thead>
+            <tr>
+                <th>First Name</th>
+                <th>Last Name</th>
+                <th>City</th>
+                <th>State</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php while($row = mysqli_fetch_array($result)):?>
+            <tr>
+                <td><a href="orders.php?id=<?=$row['CustomerID']?>"><?=$row['FirstName']?></a></td>
+                <td><?=$row['LastName']?></td>
+                <td><?=$row['City']?></td>
+                <td><?=$row['State']?></td>
+            </tr>                    
+            <?php endwhile;?>
+        </tbody>
+    <?php else:?>
+        <thead>
+            <tr>
+                <th>Order Date</th>
+                <th>Order Number</th>
+                <th>UnitPrice</th>
+                <th>Quanity</th>
+                <th>Line Total</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php $total=0; while($row = mysqli_fetch_array($result)): $total += ($row['UnitPrice'] * $row['OrderQuantity']);?>
+            <tr>
+                <td><?=$row['OrderDate']?></td>
+                <td><?=$row['OrderID']?></td>
+                <td><?=$row['UnitPrice']?></td>
+                <td><?=$row['OrderQuantity']?></td>
+                <td><?=($row['UnitPrice'] * $row['OrderQuantity'])?></td>
+            </tr>                    
+            <?php endwhile;?>
+        </tbody>
+    <?php endif;?>
+    </table>
+    <h1 class="text-center">Total for all items: $<?=$total?></h1>
 </main>
 
 <?php
